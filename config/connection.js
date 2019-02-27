464ffad7fcf0896ca77dad048e3f0310393291ee
Ignatzz <martinsandeen@gmail.com>
2019-02-27 03:19:39 -0500

    functional app. needs styling and heroku deployment

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: '0Captain',
+		database: 'burgers_db'
+	})
+};
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+
+
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Boy Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Jamon Burger', FALSE);
