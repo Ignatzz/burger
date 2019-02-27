ba0cd7c0a3cd716f936a2bd90fe5ab8b0e421bd0
Ignatzz <martinsandeen@gmail.com>
2019-02-27 18:36:20 -0500

    fixed old cat var

--- db/schema.sql
+++ db/schema.sql
@@ -4,8 +4,9 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-id int NOT NULL AUTO_INCREMENT,
+id INT AUTO_INCREMENT NOT NULL,
 burger_name varchar(255) NOT NULL,
 devoured boolean DEFAULT false,
+createdAt TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
